465aed9932c77e33bc71a4fec853e92a3211442b
Baptiste Langlade <baptouuuu@gmail.com>
2018-01-21 11:34:00 +0100

    add container builder

---
+++ src/ContainerBuilder.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Compose;
+
+use Innmind\Url\PathInterface;
+use Innmind\Immutable\MapInterface;
+use Psr\Container\ContainerInterface;
+
+final class ContainerBuilder
+{
+    private $load;
+
+    public function __construct(Loader $loader)
+    {
+        $this->load = $loader;
+    }
+
+    /**
+     * @param MapInterface<string, mixed> $arguments
+     */
+    public function __invoke(
+        PathInterface $definition,
+        MapInterface $arguments
+    ): ContainerInterface {
+        $definitions = ($this->load)($definition);
+
+        return new Container($definitions->inject($arguments));
+    }
+}
---
+++ tests/ContainerBuilderTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Compose;
+
+use Innmind\Compose\{
+    ContainerBuilder,
+    Container,
+    Loader\Yaml,
+    Definition\Argument\Types,
+    Definition\Service\Arguments
+};
+use Innmind\Url\Path;
+use Innmind\Immutable\Map;
+use PHPUnit\Framework\TestCase;
+use Fixture\Innmind\Compose\ServiceFixture;
+
+class ContainerBuilderTest extends TestCase
+{
+    public function testInvokation()
+    {
+        $build = new ContainerBuilder(
+            new Yaml(
+                new Types,
+                new Arguments
+            )
+        );
+
+        $container = $build(
+            new Path('fixtures/container/full.yml'),
+            (new Map('string', 'mixed'))
+                ->put('first', 42)
+        );
+
+        $this->assertInstanceOf(Container::class, $container);
+
+        $service = $container->get('foo');
+        $this->assertInstanceOf(ServiceFixture::class, $service);
+        $this->assertSame(42, $service->first);
+    }
+}
